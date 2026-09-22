31b3db9f728a0b69cee859ae89b96b46e6b3b749
Youssef Doumari <doumariyoussef837@gmail.com>
2026-09-22 12:06:05 +0000

    Ajout modification par le développeur

--- devoir2-git/src/main/java/com/devoir2/Main.java
+++ devoir2-git/src/main/java/com/devoir2/Main.java
@@ -3,5 +3,6 @@ package com.devoir2;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Modification faite par le développeur !");
     }
 }
